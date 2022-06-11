11de7a87b39fdb6d08756c90a0bfbabee53be6f7
bucenecdominik <Db210420033@gmail.com>
2022-06-11 20:21:06 +0200

    hovno

--- myexpenseproject/users/views.py
+++ myexpenseproject/users/views.py
@@ -1,3 +1,4 @@
+from aiohttp import request
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
@@ -6,20 +7,16 @@ from .models import Item
 from datetime import date, datetime
 
 # Create your views here.
-
 def index(request):
-    item_list = Item.objects.all()
-    
-    return render(request, "index.html", {
+    user = request.user
+    if not user: 
+        return redirect('login_user')
+    else:
+        item_list = Item.objects.all()
+        return render(request, "index.html", {
         'item_list' : item_list
-    })
+        })
 
-def items(request):
-    item_list = Item.objects.all()
-
-    return render(request, "items.html", {
-        'item_list': item_list,
-    })
 
 def delete_item(request):
     pass
@@ -34,6 +31,7 @@ def login_user(request):
             messages.success(request, f"You have successfully logged in {username}")
             login(request, user)
             return render(request, 'index.html', {
+                'user' : user,
                 'name' : username,
             })
         else:
@@ -56,28 +54,29 @@ def add_item(request):
         description = request.POST['description']
         time_it_was_bought = datetime.now()
         price = request.POST['price']
-        item = Item.objects.create(name=name, description=description, time_it_was_bought=time_it_was_bought, price=price)
+        user = request.user
+        item = Item.objects.create(name=name, description=description, time_it_was_bought=time_it_was_bought, price=price, user=user)
         item.save()
         return render(request, "add_item.html")
     else:
         return render(request, "add_item.html")
     
 def money_spent_today(request):
-    items = Item.objects.filter(time_it_was_bought__date=date.today())
+    items = Item.objects.filter(time_it_was_bought__date=date.today(), user = request.user)
 
     return render(request, 'money_spent_today.html', {
         'items' : items
     })
 
 def money_spent_this_month(request):
-    items = Item.objects.filter(time_it_was_bought__month = datetime.now().month)
+    items = Item.objects.filter(time_it_was_bought__month = datetime.now().month, user = request.user)
 
     return render(request, 'money_spent_this_month.html', {
         'items' : items
     })
 
 def money_spent_this_year(request):
-    items = Item.objects.filter(time_it_was_bought__year = datetime.now().year)
+    items = Item.objects.filter(time_it_was_bought__year = datetime.now().year, user = request.user)
 
     return render(request, 'money_spent_this_month.html', {
         'items' : items
